9914d2aa11d3e5ff6d22f52a606c7f4f97b4c2ea
Giuseppe De Marco <giuseppe.demarco@unical.it>
2019-05-19 18:50:44 +0200

    test multiple haarcascades

--- face_detect.py
+++ face_detect.py
@@ -1,37 +1,54 @@
 import cv2
+import os
 import sys
 
 # Get user supplied values
-imagePath = sys.argv[1]
+
 # in opencv/data/haarcascades
-#cascPath = "haarcascade_frontalface_default.xml"
-cascPath = "haarcascades/haarcascade_frontalcatface_extended.xml"
-#cascPath = "haarcascade_frontalface_alt.xml"
 
+casBasePath = 'haarcascades'
 
-# Create the haar cascade
-faceCascade = cv2.CascadeClassifier(cascPath)
+cascPaths = ("haarcascade_frontalface_default.xml",
+             "haarcascade_frontalcatface_extended.xml",
+             "haarcascade_profileface.xml",
+             "haarcascade_fullbody.xml",
+             "haarcascade_upperbody.xml",
+             "haarcascade_frontalface_alt.xml")
 
-# Read the image
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def face_detection(cascPath, imagePath = sys.argv[1]):
+    # Create the haar cascade
+    faceCascade = cv2.CascadeClassifier(cascPath)
+    
+    # Read the image
+    image = cv2.imread(imagePath)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+        #flags = cv2.CV_HAAR_SCALE_IMAGE
+    )
+    
+    if not len(faces):
+        return False, image
+    msg = "Found {0} faces in {1} [{2}]".format(len(faces),
+                                             imagePath,
+                                             cascPath)
+    print(msg)
+    
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    return cv2, image
 
-# Detect faces in the image
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.2,
-    minNeighbors=5,
-    minSize=(30, 30)
-    #flags = cv2.CV_HAAR_SCALE_IMAGE
-)
-
-if not len(faces): sys.exit(1)
-msg = "Found {0} faces in {1}".format(len(faces), imagePath)
-print(msg)
-
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-cv2.imshow(msg, image)
-cv2.waitKey(0)
+for i in cascPaths:
+    cascPath = os.path.join(casBasePath, i)
+    imagePath = sys.argv[1]
+    cv2, image = face_detection(cascPath, imagePath)
+    if cv2:
+        cv2.imshow(imagePath, image)
+        cv2.waitKey(0)
+        break
