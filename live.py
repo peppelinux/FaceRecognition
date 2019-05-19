78d349ea9ac90687c5bc8391674f172c40b0d5cd
Giuseppe De Marco <giuseppe.demarco@unical.it>
2019-05-19 19:40:01 +0200

    live

--- live.py
+++ live.py
@@ -2,39 +2,48 @@
 # Tested with OpenCV3
 
 import cv2
+import sys
+import time
 
-cap = cv2.VideoCapture(0)
+
+#cap = cv2.VideoCapture('http://192.168.3.35:8081')
+cap = cv2.VideoCapture(sys.argv[1])
 
 # Create the haar cascade
-faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+faceCascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
 
+print('Running ...')
 while(True):
-	# Capture frame-by-frame
-	ret, frame = cap.read()
+    # Capture frame-by-frame
+    ret, frame = cap.read()
 
-	# Our operations on the frame come here
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-	# Detect faces in the image
-	faces = faceCascade.detectMultiScale(
-		gray,
-		scaleFactor=1.1,
-		minNeighbors=5,
-		minSize=(30, 30)
-		#flags = cv2.CV_HAAR_SCALE_IMAGE
-	)
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(30, 30)
+        #flags = cv2.CV_HAAR_SCALE_IMAGE
+    )
 
-	print("Found {0} faces!".format(len(faces)))
+    if not len(faces):
+        time.sleep(0.2)
+        continue
 
-	# Draw a rectangle around the faces
-	for (x, y, w, h) in faces:
-		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    print("Found {0} faces!".format(len(faces)))
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 
-	# Display the resulting frame
-	cv2.imshow('frame', frame)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 # When everything done, release the capture
 cap.release()
